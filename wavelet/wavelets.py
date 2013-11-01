2081db7b0f5ea7649db38b3b4279c4ef0d82840d
EvanBianco <evan@agilegeoscience.com>
2013-11-01 12:34:50 -0300

    made all the wavelets a function of duration (time window) and sample interval (dt), and made LFM and Klauder one function (sweep)

--- wavelet/wavelets.py
+++ wavelet/wavelets.py
@@ -1,7 +1,96 @@
+# -*- coding: utf-8 -*-
 import numpy as np
 from enthought.traits.api import HasTraits,Array,Float, Range, Int, Event
 from scipy.signal import hilbert
 from enthought.traits.ui.api import View,Item, RangeEditor
+
+def ricker( duration, dt, f ):
+    """
+    Also known as the mexican hat wavelet, models the function:
+    A =  (1-2 \pi^2 f^2 t^2) e^{-\pi^2 f^2 t^2}
+
+    :param duration: The length in seconds of the wavelet.
+    :param dt: is the sample interval in seconds (usually 0.001, 0.002, 0.004)
+    :params f: Center frequency of the wavelet (in Hz)
+
+    :returns: The ricker wavelet with center frequency f sampled at t.
+    """
+    t = np.arange( -duration/2, duration/2 , dt) 
+    pi2 = (np.pi ** 2.0)
+    fsqr = f ** 2
+    tsqr = t ** 2
+    pft = pi2 * fsqr * tsqr
+    A = (1 - (2 * pft)) * np.exp(-pft)
+
+    return A
+    
+    
+def sweep( duration, dt, f1, f2, method = 'linear', phi = 0, vertex_zero = True, 
+    autocorrelate = True ):
+    """
+    Generates a linear frequency modulated wavelet (sweep)
+    Does a wrapping of scipy.signal.chirp
+    :param duration: The length in seconds of the wavelet.
+    :param dt: is the sample interval in seconds (usually 0.001, 0.002, 0.004)
+    :param f1: The start frequency of the wavelet.
+    :param f2: The end frequency of the wavelet (in Hz)
+    :keyword method: {'linear','quadratic','logarithmic'}, optional
+    :keyword phi: float, phase offset in degrees
+    :keyword vertex_zero: bool, optional
+        This parameter is only used when method is ‘quadratic’. 
+        It determines whether the vertex of the parabola that 
+        is the graph of the frequency is at t=0 or t=t1.
+
+    :returns: An LFM waveform.
+    """
+    
+    t = np.arange( -duration/2, duration/2 , dt) 
+    t0 = -duration/2
+    t1 = duration/2
+    from scipy.signal import chirp
+    A = chirp( t , f1, t1, f2, method, phi, vertex_zero  )
+    
+    if autocorrelate:
+        A = np.correlate(A, A, mode='same')
+    
+    return A
+
+
+def ormsby(duration, dt, f1, f2, f3, f4):
+    """
+    The Ormsby wavelet requires four frequencies:
+    f1 = low-cut frequency
+    f2 = low-pass frequency
+    f3 = high-pass frequency
+    f4 = hi-cut frequency
+    Together, the frequencies define a trapezoid shape in the spectrum.
+    The Ormsby wavelet has several sidelobes, unlike Ricker wavelets
+    which only have two, one either side.
+
+    :param duration: The length in seconds of the wavelet.
+    :param dt: is the sample interval in seconds (usually 0.001, 0.002, 0.004)
+    :params f1: Low cut frequency
+    :params f2: Low pass frequency
+    :params f3: High pass frequency
+    :params f4: High cut frequency
+
+    :returns: A vector containing the ormsby wavelet
+    """
+    
+    def numerator(f,t):
+        return (np.sinc( f * t)**2) * ((np.pi * f) ** 2)
+
+    pf43 = ( np.pi * f4 ) - ( np.pi * f3 )
+    pf21 = ( np.pi * f2 ) - ( np.pi * f1 )
+    
+    t = np.arange( -duration/2, duration/2 , dt) 
+    
+    A = ( ( numerator(f4,t)/pf43 ) - ( numerator(f3,t)/pf43 ) -
+         ( numerator(f2,t)/pf21 ) + ( numerator(f1,t)/pf21) )
+
+    A /= np.amax( A )
+    return A
+
 
 def rotate_phase( w, phi ):
     """
@@ -20,104 +109,6 @@ def rotate_phase( w, phi ):
 
     A = (np.real( a ) * np.cos( phi ) +
          np.imag( a  ) * np.sin( phi ) )
-
-    return A
-    
-def lfm( duration,fs,f1,f2 ):
-    """
-    Generates a linear frequency modulated wavelet.
-    :param duration: The length in seconds of the wavelet.
-    :param f1: The start frequency of the wavelet.
-    :param f2: The end frequency of the wavelet.
-
-    :returns: An LFM waveform.
-    """
-
-    k = (f2 - f1) / duration
-    n = duration * fs
-    t = np.linspace( -.5,.5,n) * duration
-    A = np.sin( 2. * np.pi * ( (f1 * t) +
-                               ( k * ( np.power(t, 2) / 2.) )) )
-
-    return A
-
-
-
-def klauder(points, f_lower, f_upper):
-    """
-    The autocorrelation of a linearly swept frequency-modulated sinuusoidal signal used in Vibroseis.
-    It is zero-phase, and symmetrical about time t = 0 ms.
-    Requires f_upper and f_lower, and a duration, which we can interpret from points.
-    """
-    
-    T = points * 0.001
-    t = np.linspace(-.1, .1, points)
-    k = (f_lower + f_upper) / T
-    f0 = (f_lower + f_upper) / 2.0
-    i = (0 + 1j)
-    A = np.real( np.sin(np.pi * k * t * (T - t))/ \
-                 (np.pi * k * t * np.exp(2.0 * np.pi * f0 * t * i)))
-
-    return A
-
-def ormsby_alg(duration, nsamps, f1,f2,f3,f4):
-    """
-    The Ormsby wavelet requires four frequencies:
-    f1 = low-cut frequency
-    f2 = low-pass frequency
-    f3 = high-pass frequency
-    f4 = hi-cut frequency
-    Together, the frequencies define a trapezoid shape in the spectrum.
-    The Ormsby wavelet has several sidelobes, unlike Ricker wavelets
-    which only have two, one either side.
-
-    :params duration: Length of the wavelet
-    :params fs: The sample rate of the date (samples/second)
-    :params f1: Low cut frequency
-    :params f2: Low pass frequency
-    :params f3: High pass frequency
-    :params f4: High cut frequency
-
-    :returns: A vector containing the ormsby wavelet
-    """
-    
-    def numerator(f,t):
-        return (np.sinc( f * t)**2) * ((np.pi * f) ** 2)
-
-    pf43 = ( np.pi * f4 ) - ( np.pi * f3 )
-    pf21 = ( np.pi * f2 ) - ( np.pi * f1 )
-    
-    n = nsamps
-    t = np.linspace( -.5,.5,n) * duration
-    
-    A = ( ( numerator(f4,t)/pf43 ) - ( numerator(f3,t)/pf43 ) -
-         ( numerator(f2,t)/pf21 ) + ( numerator(f1,t)/pf21) )
-
-    A /= np.amax( A )
-    return A
-
-
-def ricker_alg( duration, nsamps, f ):
-    """
-    Also known as the mexican hat wavelet, models the function:
-    A =  (1-2 \pi^2 f^2 t^2) e^{-\pi^2 f^2 t^2}
-
-    :params duration: Length of the wavelet
-    :params nsamps: The number of samples to take. 
-                    nsamps/duration = sample rate
-    :params f: Center frequency of the wavelet.
-
-    :returns: The ricker wavelet with center frequency f sampled at t.
-    """
-    t = np.linspace( -.5,.5, nsamps ) * duration
-    
-    pi2 = (np.pi ** 2.0)
-    fsqr = f ** 2
-    tsqr = t ** 2
-
-    pft = pi2 * fsqr * tsqr
-    
-    A = (1 - (2 * pft)) * np.exp(-pft)
 
     return A
 
