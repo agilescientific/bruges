48b376681b1917442a91bde5e65e9e2a88c53836
EvanBianco <evan@agilegeoscience.com>
2013-11-01 13:37:52 -0300

    change lfm to sweep in __init__

--- wavelet/__init__.py
+++ wavelet/__init__.py
@@ -1,2 +1,2 @@
-from wavelets import ricker, ormsby, lfm, rotate_phase, Ricker, Ormsby, Wavelet
+from wavelets import ricker, ormsby, sweep, rotate_phase, Ricker, Ormsby, Wavelet
 __all__ = ["ricker", "ormsby", "sweep", "rotate_phase", "Ricker", "Ormsby" ]
