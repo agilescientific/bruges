5e0d6353a252465d48957c33f117deb001be40f8
ben-bougher <ben.bougher@gmail.com>
2013-11-11 17:08:37 -0400

    script for finding and running all the unit tests

---
+++ run_tests.py
@@ -0,0 +1,22 @@
+import unittest
+import os
+
+def run_tests():
+
+    tests = []
+    for root,dirs,files in os.walk( 'agilegeo' ):
+
+        if 'test' in root:
+            tests.append(
+                unittest.TestLoader().discover(root,
+                                             pattern='*test.py'))
+
+    suite = unittest.TestSuite( tests )
+
+    return(suite)
+
+if ( __name__ == '__main__' ):
+    
+    suite = run_tests()
+    unittest.TextTestRunner(verbosity=2).run(suite)
+
