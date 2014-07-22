4ef78046f793d613bad67221169c958e89067477
kwinkunks <matt@agilegeoscience.com>
2014-07-22 17:05:13 -0300

    added fluidsub, test failing

--- agilegeo/avo/__init__.py
+++ agilegeo/avo/__init__.py
@@ -9,11 +9,14 @@ from reflection import zoeppritz, akirichards, akirichards_alt, \
 from timedepthconv import time_to_depth, depth_to_time
 from moduli import youngs, bulk, pr, mu, lam, \
                    pmod, vp, vs, moduli
+from fluidsub import gassmann, fluidsub
+
 
 __all__=[ "zoeppritz", "akirichards", "akirichards_alt",
           "fatti", "shuey2", "shuey3", "bortfeld2", "bortfeld3",
           "time_to_depth", "depth_to_time",
           "youngs", "bulk", "pr",
           "mu", "lam", "pmod", 
-          "vp", "vs", "moduli"
+          "vp", "vs", "moduli",
+          "gassmann", "fluidsub"
         ]
