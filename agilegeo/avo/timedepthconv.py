88eebc171278a925cacfc4bd226b2e4fd4bba9e8
kwinkunks <matt@agilegeoscience.com>
2015-03-24 17:38:17 -0300

    what I have right now

--- agilegeo/avo/timedepthconv.py
+++ agilegeo/avo/timedepthconv.py
@@ -15,29 +15,26 @@ def __convert(data, vmodel, interval, interval_new, scale, mode):
     if len(data.shape) == 1:
         ntraces = 1
         nsamps = data.size
-
         if size(dz) == 1:
             depths = arange(nsamps)*dz
         else:
             depths = dz
-
         v_avg = cumsum(vmodel) / (arange(nsamps) + 1)
 
     else:
         ntraces = data.shape[-1]
         nsamps = data.shape[0]
-
         if size(dz) == 1:
             tr = [(arange(nsamps) * dz) for i in range(ntraces)]
             depths = transpose(asarray(tr))
         else:
             depths = dz
-
         tr = [arange(nsamps) + 1 for i in range(ntraces)]
         v_avg = cumsum(vmodel, axis=0) / transpose(tr)
 
     times = depths / v_avg
     times *= scale
+
     if size(dt) == 1:
         times_lin = arange(amin(times), amax(times), dt)
     else:
@@ -48,16 +45,16 @@ def __convert(data, vmodel, interval, interval_new, scale, mode):
                          bounds_error=False,
                          fill_value=data[-1],
                          kind='nearest')
-        return(inter(times_lin))
-
-    output = zeros((times_lin.size, ntraces))
-    for i in range(ntraces):
-        inter = interp1d(times[:, i], data[:, i],
-                         bounds_error=False,
-                         fill_value=data[-1, i],
-                         kind=mode)
-        output[:, i] += inter(times_lin)
-    return output
+        return inter(times_lin)
+    else:
+        output = zeros((times_lin.size, ntraces))
+        for i in range(ntraces):
+            inter = interp1d(times[:, i], data[:, i],
+                             bounds_error=False,
+                             fill_value=data[-1, i],
+                             kind=mode)
+            output[:, i] += inter(times_lin)
+        return output
 
 
 def time_to_depth(data, vmodel, dt, dz, twt=True,
