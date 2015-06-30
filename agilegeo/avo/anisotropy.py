78277cabb19054770bee7a39ef5a342e224875ae
kwinkunks <matt@agilegeoscience.com>
2015-06-30 11:41:15 -0300

    added anisotropy module, untested

---
+++ agilegeo/avo/anisotropy.py
@@ -0,0 +1,146 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Thin-layer anisotropy.
+
+:copyright: 2015 Agile Geoscience
+:license: Apache 2.0
+"""
+import numpy as np
+
+import moduli
+from utility import moving_average
+
+
+def backus_parameters(vp, vs, rho, lb, dz):
+    """
+    Backus parameters.
+
+    Liner, C (2014), Long-wave elastic attenuation produced by horizontal
+        layering. The Leading Edge, June 2014, p 634-638.
+
+    """
+    lam = moduli.lam(vp, vs, rho)
+    mu = moduli.mu(vp, vs, rho)
+
+    # Compute the layer parameters from Liner (2014) equation 2:
+    a = rho * np.power(vp, 2.0)  # Acoustic impedance
+
+    # Compute the Backus parameters from Liner (2014) equation 4:
+    A1 = 4 * moving_average(mu*(lam+mu)/a, lb/dz, mode='same')
+    A = A1 + np.power(moving_average(lam/a, lb/dz, mode='same'), 2.0)\
+        / moving_average(1.0/a, lb/dz, mode='same')
+    C = 1.0 / moving_average(1.0/a, lb/dz, mode='same')
+    F = moving_average(lam/a, lb/dz, mode='same')\
+        / moving_average(1.0/a, lb/dz, mode='same')
+    L = 1.0 / moving_average(1.0/mu, lb/dz, mode='same')
+    M = moving_average(mu, lb/dz, mode='same')
+
+    return A, C, F, L, M
+
+
+def backus(vp, vs, rho, lb, dz):
+    """
+    Backus averaging.
+
+    Liner, C (2014), Long-wave elastic attenuation produced by horizontal
+        layering. The Leading Edge, June 2014, p 634-638.
+
+    """
+    # Compute the Backus parameters:
+    A, C, F, L, M = backus_parameters(vp, vs, rho, lb, dz)
+
+    # Compute the vertical velocities from Liner (2014) equation 5:
+    R = moving_average(rho, lb/dz, mode='same')
+    vp0 = np.sqrt(C / R)
+    vs0 = np.sqrt(L / R)
+
+    return vp0, vs0
+
+
+def quality_factor(vp, vs, rho, lb, dz):
+    """
+    Compute Qp and Qs from Liner (2014) equation 10.
+
+    """
+    vp0, vs0 = backus(vp, vs, rho, lb, dz)
+
+    ptemp = np.pi * np.log(vp0 / vp) / (np.log(vp0 / vp) + np.log(lb/dz))
+    Qp = 1.0 / np.tan(ptemp)
+
+    stemp = np.pi * np.log(vs0 / vs) / (np.log(vs0 / vs) + np.log(lb/dz))
+    Qs = 1.0 / np.tan(stemp)
+
+    return Qp, Qs
+
+
+def thomsen_parameters(vp, vs, rho, lb, dz):
+    """
+    Liner, C, and T Fei (2006). Layer-induced seismic anisotropy from
+    full-wave sonic logs: Theory, application, and validation.
+    Geophysics 71 (6), p D183–D190. DOI:10.1190/1.2356997
+
+    """
+    A, C, F, L, M = backus_parameters(vp, vs, rho, lb, dz)
+
+    delta = ((F + L)**2.0 - (C - L)**2.0) / (2.0 * C * (C - L))
+    epsilon = (A - C) / (2.0 * C)
+    gamma = (M - L) / (2.0 * L)
+
+    return delta, epsilon, gamma
+
+
+def dispersion_parameter(qp):
+    """
+    Kjartansson (1979). Journal of Geophysical Research, 84 (B9),
+    4737-4748. DOI: 10.1029/JB084iB09p04737.
+    """
+    return np.arctan(1/qp) / np.pi
+
+
+def blangy(vp0, vs0, rho0, d0, e0, vp1, vs1, rho1, d1, e1, theta):
+    """
+    TODO
+        Use rocks.
+    """
+    # I already had code set up like this, so build some convenience dicts.
+    # These should be rock objects.
+    lower = {'vp': vp0,
+             'vs': vs0,
+             'rho': rho0,
+             'd': d0,
+             'e': e0,
+             }
+
+    upper = {'vp': vp1,
+             'vs': vs1,
+             'rho': rho1,
+             'd': d1,
+             'e': e1,
+             }
+
+    # Redefine theta
+    inc_angle = np.radians(theta)
+    trans_angle = np.arcsin(np.sin(inc_angle) * lower['vp']/upper['vp'])
+    theta = 0.5 * (inc_angle + trans_angle)
+
+    vp = (upper['vp'] + lower['vp'])/2.0
+    vs = (upper['vs'] + lower['vs'])/2.0
+    rho = (upper['rho'] + lower['rho'])/2.0
+
+    dvp = lower['vp'] - upper['vp']
+    dvs = lower['vs'] - upper['vs']
+    drho = lower['rho'] - upper['rho']
+    dd = lower['d'] - upper['d']
+    de = lower['e'] - upper['e']
+
+    A = 0.5 * (drho/rho + dvp/vp)
+    B = 2.0 * (vs**2 / vp**2) * ((drho/rho + 2 * dvs/vs)) * np.sin(theta)**2
+    C = 0.5 * (dvp/vp) * np.tan(theta)**2
+    D = 0.5 * dd * np.sin(theta)**2
+    E = 0.5 * (dd - de) * np.sin(theta)**2 * np.tan(theta)**2
+
+    isotropic = A - B + C
+    anisotropic = A - B + C + D - E
+
+    return isotropic, anisotropic
--- agilegeo/util/utility.py
+++ agilegeo/util/utility.py
@@ -1,10 +1,15 @@
-import scipy as sp
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Utility functions.
+
+:copyright: 2015 Agile Geoscience
+:license: Apache 2.0
+"""
+import scipy.signal
 import numpy as np
 
-"""
-These are generally useful function calls that don't necessarily fit
-into a specific geophysics
-"""
+
 def rms(a):
     """
     Calculates the RMS of an array.
@@ -18,9 +23,71 @@ def rms(a):
     return np.sqrt(np.sum(a**2.0)/a.size)
 
 
+def moving_average(a, length, mode='valid'):
+    """
+    Computes the mean in a moving window. Naive implementation.
+
+    Example:
+        >>> test = np.array([1,9,9,9,9,9,9,2,3,9,2,2,3,1,1,1,1,3,4,9,9,9,8,3])
+        >>> moving_average(test, 7, mode='same')
+        [ 4.4285714  5.571428  6.7142857  7.8571428  8.          7.1428571
+          7.1428571  6.142857  5.1428571  4.2857142  3.1428571  3.
+          2.7142857  1.571428  1.7142857  2.          2.857142  4.
+          5.1428571  6.142857  6.4285714  6.1428571  5.7142857  4.5714285 ]
+
+    TODO:
+        Other types of average.
+
+    """
+    pad = np.floor(length/2)
+
+    if mode == 'full':
+        pad *= 2
+
+    # Make a padded version, paddding with first and last values
+    r = np.empty(a.shape[0] + 2*pad)
+    r[:pad] = a[0]
+    r[pad:-pad] = a
+    r[-pad:] = a[-1]
+
+    # Cumsum with shifting trick
+    s = np.cumsum(r, dtype=float)
+    s[length:] = s[length:] - s[:-length]
+    out = s[length-1:]/length
+
+    # Decide what to return
+    if mode == 'same':
+        if out.shape[0] != a.shape[0]:
+            # If size doesn't match, then interpolate.
+            out = (out[:-1, ...] + out[1:, ...]) / 2
+        return out
+    elif mode == 'valid':
+        return out[pad:-pad]
+    else:  # mode=='full' and we used a double pad
+        return out
+
+
+def moving_avg_conv(a, length):
+    """
+    Moving average via convolution. Seems slower than naive.
+
+    """
+    boxcar = np.ones(length)/length
+    return np.convolve(a, boxcar, mode="same")
+
+
+def moving_avg_fft(a, length):
+    """
+    Moving average via FFT convolution. Seems slower than naive.
+
+    """
+    boxcar = np.ones(length)/length
+    return scipy.signal.fftconvolve(a, boxcar, mode="same")
+
+
 def normalize(a, new_min=0.0, new_max=1.0):
     """
-    Normalize an array to [0,1] or to 
+    Normalize an array to [0,1] or to
     arbitrary new min and max.
 
     :param a: An array.
@@ -29,7 +96,7 @@ def normalize(a, new_min=0.0, new_max=1.0):
 
     :returns: The normalized array.
     """
-    
+
     n = (a - np.amin(a)) / np.amax(a - np.amin(a))
     return n * (new_max - new_min) + new_min
 
@@ -52,23 +119,59 @@ def noise_db(a, snr):
     Takes an array of seismic amplitudes
     and SNR in dB.
 
-    Returns an array of noise, the same 
+    Returns an array of noise, the same
     shape as the input.
 
     Note it does *not* return the input
     array with the noise added.
 
     """
-    
+
     # Get the amplitude of the signal
     sigmean = rms(a)
 
     # Calculate the amp of the noise,
     # given the desired SNR
     noisemean = sigmean / 10.0**(snr/20.0)
-    
+
     # Normal noise, centered on 0,
     # SD=sqrt(var), same shape as input
     noise = noisemean * np.random.normal(0.0, 1.0, a.shape)
-    
+
     return noise
+
+
+def top_and_tail(a, b=np.array([]), c=np.array([])):
+    """
+    Top and tail up to 3 arrays to the non-NaN extent of the first array.
+
+    E.g. crop the NaNs from the top and tail of a well log.
+
+    TODO:
+        Make this work for an arbitrary number of arrays.
+
+    """
+    nans = np.where(~np.isnan(a))[0]
+    first, last = nans[0], nans[-1]
+    a = a[first:last]
+    if b.any():
+        b = b[first:last]
+        if c.any():
+            c = c[first:last]
+            return a, b, c
+        return a, b
+    return a
+
+
+def extrapolate(a):
+    """
+    Extrapolate up and down an array from the first and last non-NaN samples.
+
+    E.g. Continue the first and last non-NaN values of a log up and down.
+
+    """
+    nans = np.where(~np.isnan(a))[0]
+    first, last = nans[0], nans[-1]
+    a[:first] = a[first]
+    a[last + 1:] = a[last]
+    return a
