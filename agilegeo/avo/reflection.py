dc40facd4de627ec4aadd294b16aa9664e35778f
EvanBianco <evan@agilegeoscience.com>
2015-07-02 15:39:46 -0300

    added elastic_impedance function to reflection.py

--- agilegeo/avo/reflection.py
+++ agilegeo/avo/reflection.py
@@ -482,3 +482,28 @@ def bortfeld3(vp1, vs1, rho1, vp2, vs2, rho2, theta1):
             sin(radians(theta1))**2
 
     return term1 + term2 + term3
+
+def elastic_impedance(vp, vs, rho, theta1, normalize='True'):
+    """
+    returns the elastic impedance (as defined by Connolly, 1999)
+    for each incidence angle in theta1:
+    :param vp1: The p-wave velocity or p-wave velocity array.
+    :param vs1: The s-wave velocity of s-wave velocity array.
+    :param rho1: The density (either scalar or array).
+    :param theta1: An array of incident angles to use for reflectivity
+                   calculation [degrees].
+    :param normalized: if 'True', returns the normalized form from 
+                    Whitcombe et. al (2001).
+    """
+    k = (vs / vp)**2  # average over interval of interest
+    a = 1 + (np.tan(theta1))**2
+    b = -8 * k * ((np.sin(theta1)) ** 2)
+    c = 1 - 4 * k * (np.sin(theta1))** 2)
+
+    EI = (vp**a)*(vs**b)*(rho**c)
+
+    if normalize:
+        N = vp**(1-a)*vs**(-b)*rho**(1-c)
+        EI = N * EI
+
+    return EI
