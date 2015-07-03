fixed typos in EI formula

agilegeo/avo/reflection.py
@@ -403,18 +403,17 @@ def elastic_impedance(vp, vs, rho, theta1, normalize='True'):
     :param rho1: The density (either scalar or array).
     :param theta1: An array of incident angles to use for reflectivity
                    calculation [degrees].
-    :param normalized: if 'True', returns the normalized form from 
+    :param normalized: if 'True', returns the normalized form from
                     Whitcombe et. al (2001).
     """
-    k = (np.mean(vs) / np.mean(vp))**2  # average over interval of interest
-    a = 1 + (np.tan(theta1))**2
+    k = (np.mean(vs) / np.mean(vp)) ** 2  # avg over interval of interest
+    a = 1 + (np.tan(theta1)) ** 2
     b = -8 * k * ((np.sin(theta1)) ** 2)
-    c = 1 - 4 * k * (np.sin(theta1))** 2)
-
-    EI = (vp**a)*(vs**b)*(rho**c)
+    c = 1 - 4 * k * ((np.sin(theta1)) ** 2)
+    ei = (vp ** a) * (vs ** b) * (rho ** c)
 
     if normalize:
-        N = vp**(1-a)*vs**(-b)*rho**(1-c)
-        EI = N * EI
+        n = vp ** (1 - a) * vs ** (-b) * rho ** (1 - c)
+        ei = n * ei
 
-    return EI
+    return ei