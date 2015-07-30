2b430997ccc362c82267866223ac1bf09bc59791
EvanBianco <evan@agilegeoscience.com>
2015-07-30 10:30:27 -0300

    tests for HS bounds passing based on Mavko Fig 2.1

--- bruges/rockphysics/test/bounds_test.py
+++ bruges/rockphysics/test/bounds_test.py
@@ -10,16 +10,26 @@ from bruges.rockphysics import bounds as b
 # Inputs.
 f0 = [0.5, 0.5]
 f1 = [50, 50]
-f2 = [0.6, 0.4]    # suspension of quartz in water
-k1 = [36.0, 4.0]   # Kquartz, Kclay
-k2 = [36.0, 2.2]   # Kquartz, Kwater
+f2 = [0.6, 0.4]     # suspension of quartz in water
+k1 = [36.0, 4.0]    # Kquartz, Kclay [GPa]
+k2 = [36.0, 2.2]    # Kquartz, Kwater [GPa]
 
+# Hashin-Shtrikman tests
+fh1 = [0.0, 1.0]     # end-member test case for HS bounds
+fh2 = [1.0, 1.0]     # end-member test case for HS bounds
+fh3 = [0.4, 0.6]    # middle-ish test case for HS bounds
+khs = [21.0, 76.8]  # Bulk moduli [GPa] for HS tests, Clay, Calcite
+ghs = [7.0, 32.0]   # Shear moduli [GPa] for HS tests, Clay, Calcite
 
 # Expected outputs.
-mv = 20.0    # Voigt average (hand-calculated)
-mr = 7.199   # Reuss average (hand-calculated)
-mr2 = 5.04   # Example from Mavko et al. RPH
-mh = 13.599  # Voigt-Reuss Hill average (hand-calculated)
+mv = 20.0       # Voigt average (hand-calculated)
+mr = 7.199      # Reuss average (hand-calculated)
+mr2 = 5.04      # Example from Mavko et al. RPH
+mh = 13.599     # Voigt-Reuss Hill average (hand-calculated)
+mhs1 = 76.8     # HS upper and lower bound at end-member 1
+mhs2 = 21.0     # HS upper and lower bound at end-member 2
+mhs3 = 35.5329  # HS lower bound at 50-50
+mhs4 = 40.3989  # HS upper bound at 50-50
 
 
 class BoundsTest(unittest.TestCase):
@@ -39,6 +49,26 @@ class BoundsTest(unittest.TestCase):
 
     def test_hill_average(self):
         self.assertAlmostEqual(b.hill_average(f0, k1), mh, places=-2)
+
+    def test_hashin_shtrikman(self):
+        self.assertAlmostEqual(
+            b.hashin_shtrikman(fh1, khs, ghs, bound='upper', moduli='bulk'), 
+            mhs1, places=-2)
+        self.assertAlmostEqual(
+            b.hashin_shtrikman(fh1, khs, ghs, bound='lower', moduli='bulk'), 
+            mhs1, places=-2)
+        self.assertAlmostEqual(
+            b.hashin_shtrikman(fh2, khs, ghs, bound='upper', moduli='bulk'),
+            mhs2, places=-2)
+        self.assertAlmostEqual(
+            b.hashin_shtrikman(fh2, khs, ghs, bound='lower', moduli='bulk'),
+            mhs2, places=-2)
+        self.assertAlmostEqual(
+            b.hashin_shtrikman(fh3, khs, ghs, bound='upper', moduli='bulk'), 
+            mhs3, places=-2)
+        self.assertAlmostEqual(
+            b.hashin_shtrikman(fh3, khs, ghs, bound='lower', moduli='bulk'), 
+            mhs3, places=-2)
 
 if __name__ == '__main__':
 
