b671b31ce87a0152938701ba6e24a487e625015e
Evan Bianco <evan@agilegeoscience.com>
2017-11-17 07:55:17 -0700

    rhob from porosity, and slowness to velocity equations

--- bruges/petrophysics/petrophysics.py
+++ bruges/petrophysics/petrophysics.py
@@ -23,3 +23,39 @@ def gardner(vp, alpha=0.31, beta=0.25):
         rhob estimate.
     '''
     return alpha * vp ** beta
+
+
+def convert_rhob_to_SI(rhob):
+    return rhob*1000
+
+
+def rhob(phi_rhob, Rho_matrix= 2650.0, Rho_fluid=1000.0):
+    """
+    Rho_matrix (sandstone) : 2650 g/cc
+    Rho_matrix (Limestome): 2710 g/cc
+    Rho_matrix (Dolomite): 2876 g/cc
+    Rho_matrix (Anyhydrite): 2977 g/cc
+    Rho_matrix (Salt): 20320 g/cc
+
+    Rho_fluid (fresh water): 1000 g/cc (is this more mud-like?)
+    Rho_fluid (salt water): 1100 g/cc
+    see wiki.aapg.org/Density-neutron_log_porosity
+    returns density porosity log """
+    
+    return Rho_matrix*(1 - phi_rhob) + Rho_fluid*phi_rhob
+
+def slowness_to_velocity(sonic, imperial=True, nansub=-999.25):
+    """
+    This only works for sonic in us/ft
+    """
+    if imperial = True:
+    	sonic[sonic == nansub] = np.nan
+    	sonic *= 3.2808  # units in us/m
+    	sonic /= 1e6     # units in s/m
+    	velocity = 1/sonic
+
+    else:
+    	sonic[sonic == nansub] = np.nan
+    	sonic /= 1e6     # units in s/m
+    	velocity = 1/sonic
+    return velocity
