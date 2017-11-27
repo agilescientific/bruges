1f04c60010bdedae7f024736ea3c4547278afaec
kwinkunks <matt@agilegeoscience.com>
2017-11-27 15:26:29 -0400

    Kuwahara for Matteo

--- bruges/filters/filters.py
+++ bruges/filters/filters.py
@@ -11,20 +11,70 @@ import scipy.ndimage
 from bruges.util import nearest
 
 
-def snn(horizon, size=5):
+def snn(arr, size=5):
+    """
+    Symmetric nearest neighbour, a nonlinear smoothing filter.
+    http://subsurfwiki.org/wiki/Symmetric_nearest_neighbour_filter
 
-    # TODO: See how it handles Nans,
-    # Consider removing them, interpolate over them,
-    # and put them back at end.
+    Args:
+        arr (ndarray): a 2D array, such as a seismic horizon.
+        size (int): the kernel size, e.g. 5 for 5x5. Should be odd,
+            rounded up if not.
 
+    Returns:
+        ndarray: the resulting smoothed array.
+
+    TODO:
+        See how it handles Nans, consider removing, interpolating, replacing.
+    """
     def func(this, pairs):
-        centre = this.flat[this.size // 2]
-        select = [nearest(this.flat[p], centre) for p in pairs]
+        centre = this[this.size // 2]
+        select = [nearest(this[p], centre) for p in pairs]
         return np.mean(select)
 
+    if not size // 2:
+        size += 1
+
     pairs = [[i, size**2-1 - i] for i in range(size**2 // 2)]
-    return scipy.ndimage.generic_filter(horizon,
+    return scipy.ndimage.generic_filter(arr,
                                         func,
                                         size=size,
                                         extra_keywords={'pairs': pairs}
+                                        )
+
+
+def kuwahara(arr, size=5):
+    """
+    Kuwahara, a nonlinear smoothing filter.
+    http://subsurfwiki.org/wiki/Kuwahara_filter
+
+    Args:
+        arr (ndarray): a 2D array, such as a seismic horizon.
+        size (int): the kernel size, e.g. 5 for 5x5. Should be odd,
+            rounded up if not.
+
+    Returns:
+        ndarray: the resulting smoothed array.
+
+    TODO:
+        See how it handles Nans, consider removing, interpolating, replacing.
+    """
+
+    def func(this):
+        k = np.ceil(this.shape[0] / 2)
+        t = this.reshape((k, k))
+        sub = np.array([t[:k, :k].flatten(),
+                        t[:k, k-1:].flatten(),
+                        t[k-1:, :k].flatten(),
+                        t[k-1:, k-1:].flatten()]
+                       )
+        select = sub[np.argmin(np.var(sub, axis=1))]
+        return np.mean(select)
+
+    if not size // 2:
+        size += 1
+
+    return scipy.ndimage.generic_filter(arr,
+                                        func,
+                                        size=size,
                                         )
