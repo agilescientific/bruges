b78ce15a3218294e743c1307a774c65fb18d35ed
kwinkunks <matt@agilegeoscience.com>
2018-01-29 11:48:08 -0400

    fixed docstrings and improved code

--- bruges/petrophysics/petrophysics.py
+++ bruges/petrophysics/petrophysics.py
@@ -1,62 +1,68 @@
 # -*- coding: utf-8 -*-
-'''
+"""
 ===================
 petrophysics.py
 ===================
 
-:copyright: 2016 Agile Geoscience
+:copyright: 2018 Agile Geoscience
 :license: Apache 2.0
-'''
+"""
 
 
-def gardner(vp, alpha=0.31, beta=0.25):
-    '''
-    Computes Gardner's density prediction.
-
-    SI units only.
+def gardner(vp, alpha=0.31, beta=0.25, fps=False):
+    """
+    Computes Gardner's density prediction from P-wave velocity.
 
     Args:
-        vp (this is actually slowness in ft / sec)
+        vp (ndarray): P-wave velocity in m/s.
+        alpha (float): The factor, 0.31 for m/s and 0.23 for fps.
+        beta (float): The exponent, usually 0.25.
+        fps (bool): Set to true for FPS and the equation will use the typical
+            value for alpha.
 
     Returns:
-        rhob estimate.
-    '''
+        ndarray: RHOB estimate.
+    """
+    if fps:
+        alpha = 0.23
     return alpha * vp ** beta
 
 
-def convert_rhob_to_SI(rhob):
-    return rhob*1000
-
-
-def rhob(phi_rhob, Rho_matrix= 2650.0, Rho_fluid=1000.0):
+def porosity_to_density(phi_rhob, rho_matrix, rho_fluid):
     """
-    Rho_matrix (sandstone) : 2650 g/cc
-    Rho_matrix (Limestome): 2710 g/cc
-    Rho_matrix (Dolomite): 2876 g/cc
-    Rho_matrix (Anyhydrite): 2977 g/cc
-    Rho_matrix (Salt): 20320 g/cc
+    Get density from a porosity log. Typical values:
 
-    Rho_fluid (fresh water): 1000 g/cc (is this more mud-like?)
-    Rho_fluid (salt water): 1100 g/cc
-    see wiki.aapg.org/Density-neutron_log_porosity
-    returns density porosity log """
-    
-    return Rho_matrix*(1 - phi_rhob) + Rho_fluid*phi_rhob
+        rho_matrix (sandstone) : 2650 g/cc
+        rho_matrix (Limestome): 2710 g/cc
+        rho_matrix (Dolomite): 2876 g/cc
+        rho_matrix (Anyhydrite): 2977 g/cc
+        rho_matrix (Salt): 20320 g/cc
 
-def slowness_to_velocity(sonic, imperial=True, nansub=-999.25):
+        rho_fluid (fresh water): 1000 g/cc (is this more mud-like?)
+        rho_fluid (salt water): 1100 g/cc
+
+    See wiki.aapg.org/Density-neutron_log_porosity.
+
+    Args:
+        phi_rhob (ndarray): The density porosity log.
+        rho_matrix (float)
+        rho_fluid (float)
+
+    Returns:
+        Estimate of RHOB.
     """
-    This only works for sonic in us/ft, always converts to SI
-    sonic :a numpy array of slowness (us/ft or us/m)
-    """
-    arr = sonic.as_matrix() 
-    if imperial:
-        arr[arr == nansub] = np.nan
-        arr *= 3.2808  # units in us/m
-        arr /= 1e6     # units in s/m
-        velocity = 1/arr
+    return rho_matrix*(1 - phi_rhob) + rho_fluid*phi_rhob
 
-    else:
-        arr[arr == nansub] = np.nan
-        arr /= 1e6     # units in s/m
-        velocity = 1/sonic
-    return velocity
+
+def slowness_to_velocity(slowness):
+    """
+    Convert a DT or DTS log in µs per unit depth, to velocity in unit depth
+    per second.
+
+    Args:
+        slowness (ndarray): A DT value or sequence of values.
+
+    Returns:
+        ndarray: The velocity.
+    """
+    return 1e6 / np.array(slowness)
