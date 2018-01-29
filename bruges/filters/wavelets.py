a3e053dfbe9c63cbe0d4584600b7a3f99d2a61d1
kwinkunks <matt@agilegeoscience.com>
2018-01-29 11:29:10 -0400

    fixes #42

--- bruges/filters/wavelets.py
+++ bruges/filters/wavelets.py
@@ -17,43 +17,31 @@ def ricker(duration, dt, f, return_t=False):
     Also known as the mexican hat wavelet, models the function:
     A =  (1-2 \pi^2 f^2 t^2) e^{-\pi^2 f^2 t^2}
 
-    :param duration: The length in seconds of the wavelet.
-    :param dt: is the sample interval in seconds (usually 0.001,
-               0.002, 0.004)
-    :params f: Center frequency of the wavelet (in Hz). If a list or tuple is
-               passed, the first element will be used.
-    :params return_t: If True, then the function returns a tuple of
-                      wavelet, time-basis, where time is the range from
-                      -durection/2 to duration/2 in steps of dt.
+    If you pass a 1D array of frequencies, you get a wavelet bank in return.
 
-    :returns: ricker wavelets with center frequency f sampled at t.
+    Args:
+        duration (float): The length in seconds of the wavelet.
+        dt (float): The sample interval in seconds (often one of  0.001, 0.002,
+            or 0.004).
+        f (ndarray): Centre frequency of the wavelet in Hz. If a sequence is
+            passed, you will get a 2D array in return, one row per frequency.
+        return_t (bool): If True, then the function returns a tuple of
+            wavelet, time-basis, where time is the range from -duration/2 to
+            duration/2 in steps of dt.
+
+    Returns:
+        ndarray. Ricker wavelet(s) with centre frequency f sampled on t.
     """
-
-    freq = np.array(f)
-
+    f = np.array(f).reshape((-1, 1))
     t = np.arange(-duration/2, duration/2, dt)
-
-    output = np.zeros((t.size, freq.size))
-
-    for i in range(freq.size):
-        pi2 = (np.pi ** 2.0)
-        if (freq.size == 1):
-            fsqr = freq ** 2.0
-        else:
-            fsqr = freq[i] ** 2.0
-        tsqr = t ** 2.0
-        pft = pi2 * fsqr * tsqr
-        A = (1 - (2 * pft)) * np.exp(-pft)
-        output[:, i] = A
-
-    if freq.size == 1:
-        output = output.flatten()
+    pft2 = (np.pi * f * t)**2
+    w = np.squeeze((1 - (2 * pft2)) * np.exp(-pft2))
 
     if return_t:
         RickerWavelet = namedtuple('RickerWavelet', ['amplitude', 'time'])
-        return RickerWavelet(output, t)
+        return RickerWavelet(w, t)
     else:
-        return output
+        return w
 
 
 def sweep(duration, dt, f, method='linear', phi=0,
@@ -102,9 +90,9 @@ def sweep(duration, dt, f, method='linear', phi=0,
 
     if return_t:
         Sweep = namedtuple('Sweep', ['amplitude', 'time'])
-        return Sweep(output, t)
+        return Sweep(output.T, t)
     else:
-        return output
+        return output.T
 
 
 def ormsby(duration, dt, f, return_t=False):
@@ -126,25 +114,12 @@ def ormsby(duration, dt, f, return_t=False):
 
     :returns: A vector containing the ormsby wavelet
     """
+    f = np.array(f)
 
-    # Try to handle some duck typing
-    if not (isinstance(f, list) or isinstance(f, tuple)):
-        f = [f]
-
-    # Deal with having fewer than 4 frequencies
-    if len(f) == 4:
-        f1 = f[0]
-        f2 = f[1]
-        f3 = f[2]
-        f4 = f[3]
-    else:
-        # Cope with only having one frequency
-        # This is an arbitrary hack, is this desirable?
-        # Need a way to notify with warnings
-        f1 = f[0]/4
-        f2 = f[0]/2
-        f3 = f[0]*2
-        f4 = f[0]*2.5
+    try:
+        f1, f2, f3, f4 = f.T
+    except ValueError:
+        raise ValueError("The last dimension must be 4")
 
     def numerator(f, t):
         return (np.sinc(f * t)**2) * ((np.pi * f) ** 2)
@@ -168,7 +143,7 @@ def ormsby(duration, dt, f, return_t=False):
 
 def rotate_phase(w, phi, degrees=False):
     """
-    Performs a phase rotation of wavelet using:
+    Performs a phase rotation of wavelet or wavelet bank using:
 
     The analytic signal can be written in the form S(t) = A(t)exp(j*theta(t))
     where A(t) = magnitude(hilbert(w(t))) and theta(t) = angle(hilbert(w(t))
@@ -179,18 +154,20 @@ def rotate_phase(w, phi, degrees=False):
     == w(t)cos(phi) - h(t)sin(phi)
 
     A = w(t)Cos(phi) - h(t)Sin(phi)
-    Where w(t) is the wavelet and h(t) is it's hilbert transform.
 
-    :params w: The wavelet vector.
-    :params phi: The phase rotation angle (in radians) to apply.
-    :params degrees: Boolean, if phi is in degrees not radians.
+    Where w(t) is the wavelet and h(t) is its Hilbert transform.
 
-    :returns: The phase rotated signal.
+    Args:
+        w (ndarray): The wavelet vector, can be a 2D wavelet bank.
+        phi (float): The phase rotation angle (in radians) to apply.
+        degrees (bool): If phi is in degrees not radians.
+
+    Returns:
+        The phase rotated signal (or bank of signals).
     """
     if degrees:
         phi = phi * np.pi / 180.0
 
-    # Get the analytic signal for the wavelet
     a = hilbert(w, axis=0)
 
     A = (np.real(a) * np.cos(phi) -
